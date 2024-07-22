58d9eb677400b1606657b0f116b1e66f6c9be672
JunYeong-J <eeot2827@gmail.com>
2024-07-22 21:47:52 +0900

    앱에 사용할 앱 이름 변경

---
+++ MyPolaroid/Sources/ProfileSettingScene/View/ProfileSettingView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileSettingView.swift
+//  MyPolaroid
+//
+//  Created by 전준영 on 7/22/24.
+//
+
+import Foundation
---
+++ MyPolaroid/Sources/ProfileSettingScene/View/ProfileSettingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileSettingViewController.swift
+//  MyPolaroid
+//
+//  Created by 전준영 on 7/22/24.
+//
+
+import Foundation
