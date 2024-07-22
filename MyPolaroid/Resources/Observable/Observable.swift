92e18373821d9b937f8b7a67f486c94630026332
JunYeong-J <eeot2827@gmail.com>
2024-07-23 02:03:52 +0900

    MVVM 셋팅

---
+++ MyPolaroid/Resources/Observable/Observable.swift
@@ -0,0 +1,28 @@
+//
+//  Observable.swift
+//  MyPolaroid
+//
+//  Created by 전준영 on 7/23/24.
+//
+
+import Foundation
+
+class Observable<T> {
+    
+    var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> Void) {
+        closure(value)
+        self.closure = closure
+    }
+}
