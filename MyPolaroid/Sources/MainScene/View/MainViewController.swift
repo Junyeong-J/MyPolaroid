ae9a39b87817f52d28e029334f306feaee52e247
JunYeong-J <eeot2827@gmail.com>
2024-07-23 17:31:47 +0900

    네비게이션 추가

--- MyPolaroid/Sources/MainScene/View/MainViewController.swift
+++ MyPolaroid/Sources/MainScene/View/MainViewController.swift
@@ -9,12 +9,18 @@ import UIKit
 
 final class MainViewController: BaseViewController<MainView> {
     
+    private let myProfileImageView = ProfileImage(profile: "profile_1", corner: 20, border: 2)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationBar(image: myProfileImageView)
     }
+    
     
     override func configureView() {
         super.configureView()
     }
+    
+    
     
 }
