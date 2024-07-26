5be9935eedbeb73cf904895cd37cc10cb12dfee8
JunYeong-J <eeot2827@gmail.com>
2024-07-26 20:34:29 +0900

    코드 변경

--- MyPolaroid/Sources/Scenes/TrendsByTopicMainScene/View/TrendsByTopicMainViewController.swift
+++ MyPolaroid/Sources/Scenes/TrendsByTopicMainScene/View/TrendsByTopicMainViewController.swift
@@ -12,10 +12,10 @@ final class TrendsByTopicMainViewController: BaseViewController<TrendsByTopicMai
     private let myProfileImageView = ProfileImage(profile: "profile_1", corner: 20, border: 2)
     
     private let viewModel = TrendsByTopicViewModel()
-    private var photoDataList = [[String : [TopicPhoto]]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setNavigationBar(image: myProfileImageView)
         configureTableView()
         bindData()
@@ -27,6 +27,7 @@ final class TrendsByTopicMainViewController: BaseViewController<TrendsByTopicMai
     
     @objc private func profileImageViewClicked() {
         let vc = ProfileSettingViewController()
+        vc.viewType = .editProfile
         navigationController?.pushViewController(vc, animated: true)
     }
     
@@ -46,7 +47,6 @@ extension TrendsByTopicMainViewController {
         viewModel.inputViewDidLoadTrigger.value = ()
         
         viewModel.outputData.bind { [weak self] data in
-            self?.photoDataList = data
             self?.rootView.tableView.reloadData()
         }
     }
@@ -54,12 +54,12 @@ extension TrendsByTopicMainViewController {
 
 extension TrendsByTopicMainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return photoDataList.count
+        return viewModel.outputData.value.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: TrendsByTopicTableViewCell.identifier, for: indexPath) as! TrendsByTopicTableViewCell
-        cell.configureData(title: Array(photoDataList[indexPath.row].keys).first ?? "")
+        cell.configureData(title: Array(viewModel.outputData.value[indexPath.row].keys).first ?? "")
         cell.collectionView.dataSource = self
         cell.collectionView.delegate = self
         cell.collectionView.tag = indexPath.row
@@ -71,13 +71,20 @@ extension TrendsByTopicMainViewController: UITableViewDelegate, UITableViewDataS
 
 extension TrendsByTopicMainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return Array(photoDataList[collectionView.tag].values).flatMap{$0}.count
+        return Array(viewModel.outputData.value[collectionView.tag].values).flatMap{$0}.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TrendsByTopicCollectionViewCell.identifier, for: indexPath) as! TrendsByTopicCollectionViewCell
-        let data = Array(photoDataList[collectionView.tag].values).flatMap{$0}[indexPath.item]
+        let data = Array(viewModel.outputData.value[collectionView.tag].values).flatMap{$0}[indexPath.item]
         cell.configureData(imageData: data)
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = PhotoDetailViewController()
+        let data = Array(viewModel.outputData.value[collectionView.tag].values).flatMap{$0}[indexPath.item]
+        vc.photoData = data
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
