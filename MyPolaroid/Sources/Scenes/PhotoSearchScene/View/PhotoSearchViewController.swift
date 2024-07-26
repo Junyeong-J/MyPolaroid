e40ab0458c12720539ebe79c99563de17b9b659c
JunYeong-J <eeot2827@gmail.com>
2024-07-26 20:32:54 +0900

    코드 개선

--- MyPolaroid/Sources/Scenes/PhotoSearchScene/View/PhotoSearchViewController.swift
+++ MyPolaroid/Sources/Scenes/PhotoSearchScene/View/PhotoSearchViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import Toast
 
 final class PhotoSearchViewController: BaseViewController<PhotoSearchView> {
+    
+    let viewModel = PhotoSearchViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +34,43 @@ extension PhotoSearchViewController {
 }
 
 extension PhotoSearchViewController: UISearchBarDelegate {
-    func updateSearchResults(for searchController: UISearchController) {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text, !searchText.isEmpty else {
+            self.view.makeToast("검색어가 잘못 입력되었습니다.")
+            return
+        }
         
+        do {
+            let _ = try validateUserInput(text: searchText)
+        } catch ValidationError.emptyString {
+            self.view.makeToast("검색어를 입력하세요")
+            return
+        } catch ValidationError.trimmingCharacters {
+            self.view.makeToast("공백만 검색이 안됩니다")
+            return
+        } catch {
+            self.view.makeToast("검색어 문제입니다.")
+            return
+        }
+        
+        print(searchText)
+        
+        viewModel.inputSearchButtonClicked.value = searchText
+    }
+    
+    func validateUserInput(text: String) throws -> Bool {
+        guard !text.isEmpty else {
+            throw ValidationError.emptyString
+        }
+        
+        guard !isOnlyWhitespace(text: text) else {
+            throw ValidationError.trimmingCharacters
+        }
+        
+        return true
+    }
+    
+    func isOnlyWhitespace(text: String) -> Bool {
+        return text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
     }
 }
--- MyPolaroid/Sources/Scenes/PhotoSearchScene/ViewModel/PhotoSearchViewModel.swift
+++ MyPolaroid/Sources/Scenes/PhotoSearchScene/ViewModel/PhotoSearchViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+final class PhotoSearchViewModel {
+    
+    var inputSearchButtonClicked: Observable<String?> = Observable(nil)
+    
+    var outputData: Observable<[PhotoSearch]> = Observable([])
+    
+    init() {
+        transform()
+    }
+    
+    private func transform() {
+        inputSearchButtonClicked.bind { [weak self] word in
+            self?.callRequest(word)
+        }
+    }
+    
+    private func callRequest(_ query: String?) {
+        guard let query = query else { return }
+        UnSplashAPIManager.shared.unSplashRequest(api: .PhotoSearchAPI(query: query), model: PhotoSearchResponse.self) { [weak self] result in
+            switch result {
+            case .success(let data):
+                print("응답 데이터: \(data)")
+                self?.outputData.value = data.results
+            case .failure(let error):
+                print("오류: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
